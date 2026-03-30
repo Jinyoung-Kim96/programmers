25ad564959c97c001686753099741d6e54c9f274
Jinyoung-Kim96 <dudwlsrla5151@gmail.com>
2026-03-30 10:59:24 +0900

    [level 1] Title: 명예의 전당 (1), Time: 10.51 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int [] answer = new int[score.length];
+        ArrayList <Integer> honor = new ArrayList<>();
+        for(int i = 0; i < score.length; i++){
+            honor.add(score[i]);
+            Collections.sort(honor);
+            if(honor.size() > k) honor.remove(0);
+            answer[i] = honor.get(0);
+                       
+        }
+        return answer;
+    }
+}
